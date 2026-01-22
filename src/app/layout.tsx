ec9144511ba37ada31be929ff22792dac3e09863
ignvensvenheim <ignas.venckunas@gmail.com>
2026-01-22 13:04:27 +0200

    favicon + title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <title>Atnaujinimai ir pakeitimai</title>
       <body className={poppins.className}>{children}</body>
     </html>
   );
